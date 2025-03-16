3ca1a3d651763a70674d7e9d9a1ade2dbbee3109
ryanpompeo <ryanpompeo05@gmail.com>
2025-03-16 17:14:13 -0300

    add favicon for web version

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'google'),
+      title: 'JOKENPO',
       debugShowCheckedModeBanner: false,
       home: Gamescreen(),
     );
